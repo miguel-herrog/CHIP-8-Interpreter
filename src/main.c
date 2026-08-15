c66edde172faa30ff91442109589c942f066445d
miguel-herrog <mig.her.rog@gmail.com>
2026-08-15 23:42:52 +0200

    feat: integrate SDL2 render loop and keyboard mapping" "Added hardware-accelerated rendering pipeline with RGBA8888 texture updates and full 16-key layout mapping for SDL_KEYDOWN/KEYUP without buffer overflows.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+#include <SDL.h>
 
 struct Chip8 {
     uint8_t RAM[4096];
@@ -280,8 +281,209 @@ void emulate_cycle(struct Chip8* cpu){
     }
 }
 
-int main() {
+
+int main(int argc, char* argv[]) {
+    // Inicializar SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("Error al inicializar SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    // Crear la Ventana (1024x512, centrada)
+    SDL_Window* window = SDL_CreateWindow(
+        "Chip-8 Emulator",
+        SDL_WINDOWPOS_CENTERED, // x 
+        SDL_WINDOWPOS_CENTERED, // y
+        1024, 512, // x, y
+        SDL_WINDOW_SHOWN
+    );
+
+    if (!window) {
+        SDL_Log("Error al crear la ventana: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // Crear el Renderer (GPU)
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED
+    );
+
+    if (!renderer) {
+        SDL_Log("Error al crear el renderer: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Crear la Textura ( RGBA, Streaming, 64x32 nativos)
+    SDL_Texture* texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_RGBA8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        64,  // Ancho nativo CHIP-8
+        32   // Alto nativo CHIP-8
+    );
+
+    if (!texture) {
+        SDL_Log("Error al crear la textura: %s", SDL_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // --- El Bucle Infinito ---
     struct Chip8 myConsole;
     initialize(&myConsole);
+    load_rom(&myConsole, "tetris.ch8"); 
+
+    int quit = 0;
+    SDL_Event event;
+
+    while (!quit) {
+        // Procesar eventos del SO
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = 1;
+            } else if(event.type == SDL_KEYDOWN){
+                switch (event.key.keysym.sym){
+                    case SDLK_1:
+                        myConsole.keypad[0x1] = 1;
+                        break;
+                    case SDLK_2:
+                        myConsole.keypad[0x2] = 1;
+                        break;
+                    case SDLK_3:
+                        myConsole.keypad[0x3] = 1;
+                        break;
+                    case SDLK_4:
+                        myConsole.keypad[0xC] = 1;
+                        break;
+                    case SDLK_q:
+                        myConsole.keypad[0x4] = 1;
+                        break;
+                    case SDLK_w:
+                        myConsole.keypad[0x5] = 1;
+                        break;
+                    case SDLK_e:
+                        myConsole.keypad[0x6] = 1;
+                        break;
+                    case SDLK_r:
+                        myConsole.keypad[0xD] = 1;
+                        break;
+                    case SDLK_a:
+                        myConsole.keypad[0x7] = 1;
+                        break;
+                    case SDLK_s:
+                        myConsole.keypad[0x8] = 1;
+                        break;
+                    case SDLK_d:
+                        myConsole.keypad[0x9] = 1;
+                        break;
+                    case SDLK_f:
+                        myConsole.keypad[0xE] = 1;
+                        break;
+                    case SDLK_z:
+                        myConsole.keypad[0xA] = 1;
+                        break;
+                    case SDLK_x:
+                        myConsole.keypad[0x0] = 1;
+                        break;
+                    case SDLK_c:
+                        myConsole.keypad[0xB] = 1;
+                        break;
+                    case SDLK_v:
+                        myConsole.keypad[0xF] = 1;
+                        break;
+                    default:
+                        break;
+                }
+            } else if(event.type == SDL_KEYUP){
+                switch (event.key.keysym.sym){
+                    case SDLK_1:
+                        myConsole.keypad[0x1] = 0;
+                        break;
+                    case SDLK_2:
+                        myConsole.keypad[0x2] = 0;
+                        break;
+                    case SDLK_3:
+                        myConsole.keypad[0x3] = 0;
+                        break;
+                    case SDLK_4:
+                        myConsole.keypad[0xC] = 0;
+                        break;
+                    case SDLK_q:
+                        myConsole.keypad[0x4] = 0;
+                        break;
+                    case SDLK_w:
+                        myConsole.keypad[0x5] = 0;
+                        break;
+                    case SDLK_e:
+                        myConsole.keypad[0x6] = 0;
+                        break;
+                    case SDLK_r:
+                        myConsole.keypad[0xD] = 0;
+                        break;
+                    case SDLK_a:
+                        myConsole.keypad[0x7] = 0;
+                        break;
+                    case SDLK_s:
+                        myConsole.keypad[0x8] = 0;
+                        break;
+                    case SDLK_d:
+                        myConsole.keypad[0x9] = 0;
+                        break;
+                    case SDLK_f:
+                        myConsole.keypad[0xE] = 0;
+                        break;
+                    case SDLK_z:
+                        myConsole.keypad[0xA] = 0;
+                        break;
+                    case SDLK_x:
+                        myConsole.keypad[0x0] = 0;
+                        break;
+                    case SDLK_c:
+                        myConsole.keypad[0xB] = 0;
+                        break;
+                    case SDLK_v:
+                        myConsole.keypad[0xF] = 0;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        // Ejecutar un ciclo de la CPU
+        emulate_cycle(&myConsole);
+
+        uint32_t pixels[64 * 32];
+        for (int i = 0; i < 64 * 32; i++){
+            if(myConsole.gfx[i] == 1){
+                pixels[i] = 0xFFFFFFFF;
+            } else{
+                pixels[i] = 0x000000FF;
+            }
+        }
+
+        SDL_UpdateTexture(
+            texture,
+            NULL,
+            pixels, 
+            64 * sizeof(uint32_t)
+        );
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+        SDL_Delay(2);
+    }
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
